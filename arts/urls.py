ed573b42e2c243bcf88c9bd87739e9d5369febde
barema4 <samrubarema6@gmail.com>
2020-10-08 19:33:02 +0300

    security and performance

--- arts/urls.py
+++ arts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PictureListView,PictureDetailView
+from .views import PictureListView,PictureDetailView,SearchResultsListView
 
 urlpatterns = [
 path('', PictureListView.as_view(), name='picture_list'),
 path('<uuid:pk>', PictureDetailView.as_view(), name='picture_detail'),
+path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
--- arts/views.py
+++ arts/views.py
@@ -1,12 +1,30 @@
+from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView,DetailView
 from .models import Picture
 
-class PictureListView(ListView):
+class PictureListView(LoginRequiredMixin,ListView):
 
     model = Picture
     template_name = 'pictures/picture_list.html'
+    login_url = 'account_login'
 
 
-class PictureDetailView(DetailView): 
+class PictureDetailView(LoginRequiredMixin,DetailView): 
     model = Picture
     template_name = 'pictures/picture_detail.html'
+    login_url = 'account_login'
+    # permission_required = 'pictures.special_status'#new
+
+
+class SearchResultsListView(ListView): # new
+    model = Picture
+
+    template_name = 'pictures/search_results.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Picture.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
+
